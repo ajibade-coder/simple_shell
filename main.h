f19d3beb50d374b50e3cc6329cc8bded4f272f54
ajibad-coder <meriesbells1996@gmail.com>
2023-09-19 23:36:31 -0700

    headerfix

--- main.h
+++ main.h
@@ -1,3 +1,17 @@
+#ifndef main_h
+#define main_h
+
+/* c libaries */
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <dirent.h>
 #include <signal.h>
 #include <stddef.h>
@@ -12,10 +26,10 @@ void trim_buffer(char *buffer);
 int main(int argc, char *argv[], char *env[]);
 size_t _strlen(const char *str);
 void child(int argc, char *argv[], char *buf,
-                char **ave, char *only, int *status);
+		char **ave, char *only, int *status);
 int _strcmp(const char *string1, const char *string2);
 void tok_buf(char *buf, char *args[], const char *del,
-                                                        char *str, char *env[]);
+		char *str, char *env[]);
 char *full_path(char *env[], char cmd[]);
 void myprintf(const char *format, ...);
 void tostring(int num);
@@ -23,17 +37,17 @@ char *path_check(char path[]);
 void _env(void);
 void _envNon(int *no_exc);
 void non_interactive(int argc, char *argv[],
-                char *env[], int *pipe);
+		char *env[], int *pipe);
 char *take_only_cmd(char **buffer, int *no_exc,
-                int argc, char *argv[], int *n_err);
+		int argc, char *argv[], int *n_err);
 void ls_check(char *ave[], char *buf, char *only);
 void change_dir(char *buffer, char *cmd, int argc, char *argv[], int *n_err);
 void comments(char **buf, int *no_exc);
 void tok(char **buf, const char **del, char **token,
-                char **str, char *args[]);
+		char **str, char *args[]);
 void handle_input_command(char **buffer, size_t *n_buffer,
-                int *no_exc, char **only_command, int status,
-                int argc, char *argv[], int *n_err);
+		int *no_exc, char **only_command, int status,
+		int argc, char *argv[], int *n_err);
 void handle_sigint(int sig_num);
 char *name_prg(int argc, char *argv[]);
 void shell_exit(int status);
